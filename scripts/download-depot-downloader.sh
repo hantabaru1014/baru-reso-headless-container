3c6dc4191ba95f5b2b683bb3c4c30e1d4150efa2
hantabaru1014 <hantabaru1014@gmail.com>
2024-12-10 15:45:51 +0000

    fix: download proper arch binary

--- scripts/download-depot-downloader.sh
+++ scripts/download-depot-downloader.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.4/DepotDownloader-linux-x64.zip -O ./DepotDownloader.zip
+if [ "$(arch)" == "aarch64" ]; then
+  wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.4/DepotDownloader-linux-arm64.zip -O ./DepotDownloader.zip
+else
+  wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.4/DepotDownloader-linux-x64.zip -O ./DepotDownloader.zip
+fi
 
 unzip ./DepotDownloader.zip
 chmod +x ./DepotDownloader
